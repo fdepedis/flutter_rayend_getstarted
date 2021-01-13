5a521097a28db2beb4b96734d19de0227c81f84a
Flavio <flavio.depedis@gmail.com>
2021-01-13 22:56:09 +0100

    Refactor and add classes to clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'dart:developer' as dev;
-import 'package:http/http.dart' as http;
-import 'package:flutter/foundation.dart';
-import 'string.dart';
+import 'strings.dart';
+import 'ghflutter.dart';
 
 void main() => runApp(GHFlutterApp());
 
@@ -14,82 +11,5 @@ class GHFlutterApp extends StatelessWidget {
       title: Strings.appTitle,
       home: GHFlutter(),
     );
-  }
-}
-
-class GHFlutter extends StatefulWidget {
-  @override
-  createState() => GHFlutterState();
-}
-
-class GHFlutterState extends State<GHFlutter> {
-  var _members = <Member>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Strings.appTitle),
-      ),
-      body: ListView.builder(
-          itemCount: _members.length * 2,
-          itemBuilder: (BuildContext context, int position) {
-            if (position.isOdd) return Divider();
-
-            final index = position ~/ 2;
-
-            return _buildRow(index);
-          }),
-    );
-  }
-
-  // _loadData() i call when the state is initialized
-  @override
-  void initState() {
-    super.initState();
-
-    _loadData();
-  }
-
-  _loadData() async {
-    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
-    http.Response response = await http.get(dataURL);
-    setState(() {
-      final membersJSON = json.decode(response.body);
-
-      for (var memberJSON in membersJSON) {
-        final member = Member(memberJSON["login"], memberJSON["avatar_url"]);
-        //dev.log('member: $member');
-        _members.add(member);
-      }
-    });
-  }
-
-  Widget _buildRow(int i) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ListTile(
-        title: Text("${_members[i].login}", style: _biggerFont),
-        leading: CircleAvatar(
-            backgroundColor: Colors.green,
-            backgroundImage: NetworkImage(_members[i].avatarUrl)),
-      ),
-    );
-  }
-}
-
-class Member {
-  final String login;
-  final String avatarUrl;
-
-  Member(this.login, this.avatarUrl) {
-    if (login == null) {
-      throw ArgumentError("login of Member cannot be null. Received: '$login'");
-    }
-    if (avatarUrl == null) {
-      throw ArgumentError(
-          "avatarUrl of Member cannot be null. Received: '$avatarUrl'");
-    }
   }
 }
